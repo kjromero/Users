893de74a38c88b83c3ba47fa7e5a8ae36b794491
Kenny Romero <sastoque.kenny@gmail.com>
2023-02-24 14:10:36 -0500

    added search view

--- users/src/main/java/com/kenny/users/presentation/composable/UsersListContent.kt
+++ users/src/main/java/com/kenny/users/presentation/composable/UsersListContent.kt
@@ -1,8 +1,14 @@
 package com.kenny.users.presentation.composable
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import com.kenny.users.entities.data.User
 
 
@@ -11,15 +17,21 @@ fun UsersListContent(
     usersList: List<User>,
     onPostsClick: (Int) -> Unit
 ) {
-    LazyColumn {
-        itemsIndexed(
-            items = usersList,
-            key = { _, user -> user.id }
-        ) { _, item ->
-            UserItem(
-                user = item,
-                onPostClick = { onPostsClick(item.id) }
-            )
+    val textState = remember { mutableStateOf(TextFieldValue("")) }
+    Column( 
+        modifier = Modifier.fillMaxSize()
+    ) {
+        SearchView(state = textState)
+        LazyColumn {
+            itemsIndexed(
+                items = usersList.filter { user -> user.name.lowercase().contains(textState.value.text.lowercase()) },
+                key = { _, user -> user.id }
+            ) { _, item ->
+                UserItem(
+                    user = item,
+                    onPostClick = { onPostsClick(item.id) }
+                )
+            }
         }
     }
 }
